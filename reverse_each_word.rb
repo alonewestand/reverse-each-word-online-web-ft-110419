9c74c3063374e238d88ef15bdd8881cb07651e05
Ken Goad <goadke@g.cofc.edu>
2019-11-08 00:29:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string="this sure is a string if I ever saw one")
   old_array = string.split(" ")
   new_array = []
-  old_array.each.collect do|string|
+  old_array.each do|string|
     new_array << string.reverse
   end
   new_array.join(" ")
